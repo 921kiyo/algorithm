fcc24c74a4cca354193a6c4a69598eb79c85f03c
Kiyohito Kunii <kk3317@line05.doc.ic.ac.uk>
2018-04-28 11:16:58 +0100

    add update

--- OS/practice.py
+++ OS/practice.py
@@ -1,1 +1,18 @@
+def hydrogen():
+    mutex.wait()
+    hydrogen += 1
+    if oxgen >= 1 and hydrogen >= 2:
+        oxgenQueue.signal()
+        oxgen -= 1
+        hydroQueue.signal(2)
+        hydrogen -= 2
+    else:
+        mutex.signal()
 
+    oxyQueue.wait()
+    hydroQueue.wait()
+
+    bond()
+
+    barrier.wait()
+    mutex.signal()
