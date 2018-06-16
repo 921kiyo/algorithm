ee4df57b78f88d22dc211c0b285e64cd031f17e8
Kiyohito Kunii (Kiyo) <kiyo921@hotmail.co.jp>
2018-06-16 10:39:41 +0100

    update

---
+++ cracking_coding/7_OOP/1_deck_of_cards/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+enum Mark { Club, Diamond, Heart, Spade};
+
+class Suit{
+  int value;
+public:
+  Suit(int v){
+    value = v;
+  }
+
+};
+
+int main(){
+  return 0;
+}
